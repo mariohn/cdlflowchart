c73e7e867d5868f9260ba9c1f99c17685db396ed
Mario <mariohn@me.com>
2025-02-06 18:22:29 -0600

    Add files via upload

---
+++ main.js
@@ -0,0 +1,7 @@
+let comboVehicle = false
+
+if  (comboVehicle === true) {
+    console.log("Does the trailer have a GVW/GVWR of 10,001 lbs. or more?")
+} else {
+    console.log("Does the single vehicle have a GVW/GVWR of 26,001 lbs. or more?")
+}

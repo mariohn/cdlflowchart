83c94d989e18d82ca3641fec3095fcaa83620d7c
Mario Martinez <mariohn@me.com>
2025-03-09 14:49:29 -0500

    Updated to new version generated by Claude Ai

--- main.js
+++
@@ -1,7 +0,0 @@
-let comboVehicle = false
-
-if  (comboVehicle === true) {
-    console.log("Does the trailer have a GVW/GVWR of 10,001 lbs. or more?")
-} else {
-    console.log("Does the single vehicle have a GVW/GVWR of 26,001 lbs. or more?")
-}
